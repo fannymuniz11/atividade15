921b45274dd08a3a0132a576231dc2023eafb294
fannymuniz11 <munizfanny03@gmail.com>
2024-09-16 09:02:17 -0300

    atividade15

---
+++ atividade15.py
@@ -0,0 +1,31 @@
+# Crie um programa que receba dois números e uma operação (soma, subtração, multiplicação ou divisão) e execute a operação desejada.
+N1 = float(input("Seu primeiro numero"))
+op = input("Qual operação deseja")
+N2 = float(Input("Seu segundo numero"))
+
+result  = 0
+
+if op == "soma"
+result = N1 + N2
+print(F"Seu resultado é {result}")
+
+elif op == "Subtração"
+result= N1- N2
+print(F"Seu resultado é {result}")
+
+elif op == "Multiplicação"
+result= N1 * N2
+print(F"Seu resultado é {result}")
+
+elif op == " Divisão"
+result= N1/N2
+print(F"Seu resultado é {result}")
+
+else:
+    print (Numero invalido)
+    
+
+
+
+
+
